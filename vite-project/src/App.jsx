94d460288957d5e715170b139969d867f88b1fa2
Ahmedabser <ahmedabser6060@gmail.com>
2025-04-22 08:19:35 +0200

    Initial commit

---
+++ vite-project/src/App.jsx
@@ -0,0 +1,14 @@
+import Navbar from "./layout/Navbar";
+
+function App() {
+  return (
+    <div>
+      <Navbar />
+      <main className="p-4">
+        <h2 className="text-2xl font-semibold text-center mt-8">Welcome to Doctor Booking UI</h2>
+      </main>
+    </div>
+  );
+}
+
+export default App;
